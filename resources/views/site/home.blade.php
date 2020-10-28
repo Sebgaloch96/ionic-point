5e8feef3d270acc69da8c0e957ded2663925fe8b
Nekoframe <frame.thanawan@gmail.com>
2020-10-28 17:27:32 +0000

    membership page styling

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -49,38 +49,7 @@
     </div>
 
 
-    <div class="homepage-contact" style="background-image: url('/images/banners/contact-section.jpg')">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 col-md-7">
-                    <div class="help-section">
-                        <h2 class="mb-4">Need help?</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <p>Visit our <a href="{{ route('contactUs') }}">Contact us</a> page for more detail.</p>
-                    </div>
-                </div>   
-                <div class="col-lg-4 offset-lg-1 col-md-5">
-                    <div class="contact-section">
-                        <div class="card">
-                            <ul class="list-unstyled">
-                                <li class="phone">
-                                    <a href="tel:0XXXXXXXXXX">0XXX XXX XXXX</a>
-                                </li>
-
-                                <li class="email">
-                                    <a href="mailto:info@ionicpoint.com">info@ionicpoint.com</a>
-                                </li>
-
-                                <li class="address">
-                                    <span>24 Lorem Ipsum road, laborum, Lincoln LN1 3SP</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('site.includes.help-section')
 
 @endsection
 
